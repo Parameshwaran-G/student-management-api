c0fb58048eff64308730230a6b9dde152ccffb30
Parameshwaran G <parameshwarangdev@gmail.com>
2026-07-25 14:12:32 +0530

    Add student retrieval and filtering endpoints

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 from pydantic import BaseModel
 from typing import Optional
 
@@ -13,6 +13,13 @@ class Student(BaseModel):
     cgpa : Optional[float] = None
     email : Optional[str] = None
 
+class StudentUpdate(BaseModel):
+    student_name : Optional[str] = None
+    department : Optional[str] = None
+    cgpa : Optional[float] = None
+    email : Optional[str] = None
+
+# create student
 @app.post("/students")
 def create_student(student : Student):
     dataBase.append(student)
@@ -20,3 +27,35 @@ def create_student(student : Student):
         "Message":"Student Data Created Successfully!!",
         "Student":student
     }
+
+# get student by id
+@app.get("/students/{student_id}")
+def get_student(student_id : int):
+    for student in dataBase:
+        if student.student_id == student_id :
+            return {
+                "Message":"Student Found Successfully!!",
+                "Student":student
+            }
+    raise HTTPException(
+        status_code=404,
+        detail="Student Not Found!!"
+    )
+
+# return students of same group (filter by group)
+@app.get("/students")
+def filter_student(student_dept:str):
+    filtered_students = []
+    for student in dataBase:
+        if student.department == student_dept :
+            filtered_students.append(student.student_name)
+    if len(filtered_students) == 0 :
+        raise HTTPException(
+            status_code=404,
+            detail="Department Does Not Found!!"
+        )
+    return {
+        "Department":student_dept,
+        "Students":filtered_students
+    }
+
