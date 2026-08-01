8bd510e191673a913b7274bdfb512b49e6c723a4
Parameshwaran G <parameshwarangdev@gmail.com>
2026-08-01 20:21:32 +0530

    feat: add field validation and refactor student model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI,HTTPException,Query
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing import Optional
 
 app = FastAPI()
@@ -7,11 +7,35 @@ app = FastAPI()
 dataBase = []
 
 class Student(BaseModel):
-    student_name : str
-    student_id : int
-    department : str
-    cgpa : Optional[float] = None
-    email : Optional[str] = None
+    student_name : str = Field(
+        ...,
+        min_length=3,
+        max_length=25,
+        description="Student Name",
+        example="Praneshwaran S"
+    )
+    student_id : int = Field(
+        ...,
+        description="Student Id",
+        example="114"
+    )
+    department : str = Field(
+        ...,
+        description="Student Department",
+        example="CSE"
+    )
+    cgpa : float = Field(
+        default="0.0",
+        ge=0,
+        le=10,
+        description="Student CGPA",
+        example="8.45"
+    )
+    email : str = Field(
+        default="student@gmail.com",
+        description="Student Email",
+        example="abc@gmail.com"
+    )
 
 class StudentUpdate(BaseModel):
     student_name : Optional[str] = None
